59439b0110d4743aca57990cf88557d985726614
Aravinthk95 <107410327+Aravinthk95@users.noreply.github.com>
2022-12-31 16:00:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+var maincontainer=document.createElement('div');
+maincontainer.setAttribute('id', 'col1');
+
+var parent=document.createElement('div');
+parent.setAttribute('class','col');
+maincontainer.append(parent);
+
+
+document.body.append(maincontainer);
+
+
+
+let timer=2008;
+let time=document.getElementById('col1');
+
+setTimeout(() =>{
+    time.className='col';
+    timer=timer+1
+    time.innerHTML=2008;
+
+    setTimeout(() =>{
+        time.className='col';
+        timer=timer+1
+        time.innerHTML=2009;
+
+      setTimeout(() =>{
+            time.className='col';
+            timer=timer+1
+            time.innerHTML=2010;
+    
+         setTimeout(() =>{
+              time.className='col';
+              timer=timer+1;
+              time.innerHTML=2011;
+             setTimeout(() =>{
+                  time.className='col';
+                  timer=timer+1;
+                  time.innerHTML=2012;
+                 setTimeout(() =>{
+                     time.className='col';
+                     timer=timer+1;
+                     time.innerHTML=2013;
+                     setTimeout(() =>{
+                         time.className='col';
+                         timer=timer+1;
+                         time.innerHTML=2014;
+                         setTimeout(() =>{
+                              time.className='col';
+                              timer=timer+1;
+                              time.innerHTML=2015;
+                             setTimeout(() =>{
+                                 time.className='col';
+                                 timer=timer+1;
+                                 time.innerHTML=2016;
+                                   setTimeout(() =>{
+                                      time.className='col';
+                                      timer=timer+1;
+                                      time.innerHTML=2017;
+                                       setTimeout(() =>{
+                                           time.className='col';
+                                           timer=timer+1;
+                                           time.innerHTML=2018;
+                                              setTimeout(() => {
+                                                    time.className='col';
+                                                    timer=timer+1;
+                                                    time.innerHTML=2019;
+                                                  setTimeout(() => {
+                                                     time.className='col';
+                                                     timer=timer+1;
+                                                     time.innerHTML=2020;
+                                                        setTimeout(() => {
+                                                            time.className='col';
+                                                            timer=timer+1;
+                                                            time.innerHTML=2021;
+                                                          setTimeout(() => {
+                                                            time.className='col';
+                                                            timer=timer+1;
+                                                            time.innerHTML=2022;
+                                                              setTimeout(() => {
+                                                                 time.className='col';
+                                                                 timer=timer+1;
+                                                                 time.innerHTML="AND SO ON YEARS";
+                                                                   setTimeout(() =>{
+                                      
+                                                                     time.className='col';
+                                                                     time.innerHTML="WISH YOU MANY MORE FABULOUS BIRTHDAY DEKSHA ! <br> & <br> WISH YOU HAPPY NEW YEAR 2023 TO ALL"
+                                                          },1000)
+                                                       },1000)
+                                                    },1000)
+                                                 },1000)
+                                              },1000)
+                                           },1000)
+                                       }, 1000)
+                                    },1000)
+                                 },1000)
+                             },1000)
+                         },1000)
+                     },1000)
+                 },1000)
+             },1000)
+         },1000)
+     },1000)    
+},1000);
